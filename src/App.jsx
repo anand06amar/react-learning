ede36dd2d24ce1dc73b5ee6e93a408d82cd74be6
anand06amar <anand07amar@gmail.com>
2024-05-04 18:31:54 +0530

    learned basic of react and usestate

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+// let counter = 5;
+const addvalue = () => {
+  setCount(count +1)
+
+// console.log("clicked",counter);
+}
+
+const removevalue = () => {
+  setCount(count - 1)
+
+// console.log("clicked",counter);
+}
+
+
+  return (
+    <>
+     <h1>counter : {count}</h1>
+     <button onClick={addvalue}>add counter</button>
+     <br />
+     <button onClick={removevalue}>remove counter</button>
+    </>
+  )
+}
+
+export default App
