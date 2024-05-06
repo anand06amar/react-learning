3e38464b0eb9b1221ad9e824af4404cc2383f228
anand06amar <anand07amar@gmail.com>
2024-05-06 22:49:30 +0530

    tailwind and props

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,31 @@ const removevalue = () => {
   return (
     <>
      <h1>counter : {count}</h1>
-     <button onClick={addvalue}>add counter</button>
+     <button className='mb-4' onClick={addvalue}>add counter</button>
      <br />
-     <button onClick={removevalue}>remove counter</button>
+     <button className='mb-4' onClick={removevalue}>remove counter</button>
+
+     <h1 className='mb-4'>tailwind</h1>
+     {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/22626143/pexels-photo-22626143/free-photo-of-a-woman-with-curly-hair-and-a-white-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512" />
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure> */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Card from './components/card.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Card username="anand" btntext="visit me"/>
+    <Card username="ashok" />
+
   </React.StrictMode>,
 )
